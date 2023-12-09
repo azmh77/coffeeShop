24cad5f87ff45e93257ed27f07e374b033cb1440
azmh77 <105504132+azmh77@users.noreply.github.com>
2023-12-09 13:30:44 +0330

    .

---
+++ src/main/java/com/rhpm/coffeeShop/service/UserService.java
@@ -0,0 +1,4 @@
+package com.rhpm.coffeeShop.service;
+
+public interface UserService {
+}
---
+++ src/main/java/com/rhpm/coffeeShop/service/serviceImpl/UserService.java
@@ -0,0 +1,7 @@
+package com.rhpm.coffeeShop.service.serviceImpl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements com.rhpm.coffeeShop.service.UserService {
+}
